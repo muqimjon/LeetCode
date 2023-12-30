2781957c4ea034c0384fb82e06c74370c7967606
Muqimjon <muqimjon.mr@gmail.com>
2023-12-31 00:47:02 +0500

    Add project files.

---
+++ src/Easy/Arrays.ConsoleApp/1.cs
@@ -0,0 +1,17 @@
+﻿namespace Arrays.ConsoleApp;
+
+internal class TwoSumSolution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numIndexMap = [];
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            int complement = target - nums[i];
+            if (numIndexMap.TryGetValue(complement, out int value))
+                return [value, i];
+            numIndexMap[nums[i]] = i;
+        }
+        return [];
+    }
+}
