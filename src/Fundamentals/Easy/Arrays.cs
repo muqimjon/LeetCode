93de23de61983988fb34e53861439bda26047c39
Muqimjon <muqimjon.mr@gmail.com>
2023-12-31 12:24:50 +0500

    🛠️ Refactor: Converted Console project to Class Library

---
+++ src/Fundamentals/Easy/Arrays.cs
@@ -0,0 +1,38 @@
+﻿namespace Easy;
+
+public class Arrays
+{
+    /// <summary>
+    /// 1. Two Sum
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numIndexMap = [];
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            int complement = target - nums[i];
+            if (numIndexMap.TryGetValue(complement, out int value))
+                return [value, i];
+            numIndexMap[nums[i]] = i;
+        }
+        return [];
+    }
+
+    /// <summary>
+    /// 26. Remove Duplicates from Sorted Array
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int RemoveDuplicates(int[] nums)
+    {
+        int uniqueCount = 1;
+        for (int i = 1; i < nums.Length; i++)
+            if (nums[i] != nums[i - 1])
+                nums[uniqueCount++] = nums[i];
+
+        return uniqueCount;
+    }
+}
