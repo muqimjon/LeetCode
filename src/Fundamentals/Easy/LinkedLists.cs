4649a2e59e0b58bdbdc47d3a3a64396c406176a6
Muqimjon <muqimjon.mr@gmail.com>
2024-01-02 11:09:36 +0500

    ✨ 21. Merge Two Sorted Lists

---
+++ src/Fundamentals/Easy/LinkedLists.cs
@@ -0,0 +1,37 @@
+﻿namespace Easy;
+
+public class LinkedLists
+{
+    #region 21. Merge Two Sorted Lists
+    /// <summary>
+    /// 21. Merge Two Sorted Lists
+    /// </summary>
+    /// <param name="list1"></param>
+    /// <param name="list2"></param>
+    /// <returns></returns>
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val) (current.next, list1) = (list1, list1.next);
+            else (current.next, list2) = (list2, list2.next);
+
+            current = current.next;
+        }
+
+        current.next = list1 ?? list2!;
+
+        return dummy.next;
+    }
+
+    #endregion
+}
+
+public class ListNode(int val = 0, ListNode next = null!)
+{
+    public int val = val;
+    public ListNode next = next;
+}
---
+++ test/Fundamentals/FundamentalEasy.Test/LinkedListsTest.cs
@@ -0,0 +1,44 @@
+﻿namespace FundamentalEasy.Test;
+
+public class LinkedListsTest
+{
+    // Arrange
+    public readonly LinkedLists Solution = new();
+
+    #region 21. Merge Two Sorted Lists
+    [Theory]
+    [InlineData(new int[] { 1, 2, 4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
+    [InlineData(new int[] { }, new int[] { }, new int[] { })]
+    [InlineData(new int[] { }, new int[] { 0 }, new int[] { 0 })]
+    public void MergeTwoLists_ShouldReturnMergedSortedLinkedList(int[] arr1, int[] arr2, int[] expectedArr)
+    {
+        // Arrange
+        ListNode list1 = CreateLinkedList(arr1);
+        ListNode list2 = CreateLinkedList(arr2);
+        ListNode expected = CreateLinkedList(expectedArr);
+
+        // Act
+        ListNode result = Solution.MergeTwoLists(list1, list2);
+
+        // Assert
+        while (result is not null || expected is not null)
+        {
+            Assert.Equal(expected.val, result!.val);
+            expected = expected.next;
+            result = result.next;
+        }
+    }
+
+    private static ListNode CreateLinkedList(int[] arr)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        foreach (int val in arr)
+        {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+    #endregion
+}
