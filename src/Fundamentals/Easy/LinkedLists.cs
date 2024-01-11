7ecfc61a0bf75480560032b0c284de14c44457b0
Muqimjon <muqimjon.mr@gmail.com>
2024-01-11 19:24:45 +0500

    🔀 83. Remove Duplicates from Sorted List

--- src/Fundamentals/Easy/LinkedLists.cs
+++ src/Fundamentals/Easy/LinkedLists.cs
@@ -4,33 +4,76 @@ public class LinkedLists
 {
     #region 21. Merge Two Sorted Lists >>
     /// <summary>
-    /// 21. Merge Two Sorted Lists
+    /// Merges two sorted linked lists into a single sorted linked list.
     /// </summary>
-    /// <param name="list1"></param>
-    /// <param name="list2"></param>
-    /// <returns></returns>
+    /// <param name="list1">The first sorted linked list.</param>
+    /// <param name="list2">The second sorted linked list.</param>
+    /// <returns>A new linked list containing the merged elements in sorted order.</returns>
     public ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        ListNode dummy = new ListNode();
-        ListNode current = dummy;
+        // Create a dummy node to simplify the code
+        ListNode dummy = new();
+        // Create a reference to the current node
+        var current = dummy;
 
-        while (list1 != null && list2 != null)
+        // Iterate until one of the lists is exhausted
+        while (list1 is not null && list2 is not null)
         {
-            if (list1.val < list2.val) (current.next, list1) = (list1, list1.next);
-            else (current.next, list2) = (list2, list2.next);
+            // Compare the values of the current nodes of both lists
+            if (list1.val < list2.val)
+                // If the value in list1 is smaller, add it to the merged list
+                (current.next, list1) = (list1, list1.next);
+            else
+                // If the value in list2 is smaller or equal, add it to the merged list
+                (current.next, list2) = (list2, list2.next);
 
+            // Move to the next node in the merged list
             current = current.next;
         }
 
+        // Add the remaining nodes from the non-empty list
         current.next = list1 ?? list2!;
 
+        // Return the merged list starting from the next of the dummy node
         return dummy.next;
     }
 
     #endregion
+
+    #region 83. Remove Duplicates from Sorted List >>
+    /// <summary>
+    /// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
+    /// </summary>
+    /// <param name="head">The head of the sorted linked list.</param>
+    /// <returns>The head of the linked list after removing duplicates.</returns>
+    /// <time>Runtime: O(n) - Linear time complexity, where n is the number of nodes in the linked list.</time>
+    /// <space>Memory: O(1) - Constant space complexity, as no additional data structures are used.</space>
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        // Check if the linked list is empty
+        if (head is null)
+            return null!;
+
+        // Create a reference to the current node
+        var current = head;
+
+        // Iterate through the linked list
+        while (head.next is not null)
+            // Check if the current node's value is equal to the next node's value
+            if (head.val.Equals(head.next.val))
+                // Skip the next node by updating the current node's next pointer
+                head.next = head.next.next;
+            else
+                // Move to the next node
+                head = head.next;
+
+        // Return the head of the modified linked list
+        return current;
+    }
+    #endregion
 }
 
-#region 21. Merge Two Sorted Lists <<
+#region ListNode Model <<
 public class ListNode(int val = 0, ListNode next = null!)
 {
     public int val = val;
--- test/Fundamentals/FundamentalEasy.Test/LinkedListsTest.cs
+++ test/Fundamentals/FundamentalEasy.Test/LinkedListsTest.cs
@@ -5,7 +5,36 @@ public class LinkedListsTest
     // Arrange
     public readonly LinkedLists Solution = new();
 
-    #region 21. Merge Two Sorted Lists
+    #region Create LinkedList <<
+    private static ListNode CreateLinkedList(int[] arr)
+    {
+        ListNode dummy = new(0);
+        ListNode current = dummy;
+        foreach (int val in arr)
+        {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+    #endregion
+
+    #region Convert LinkedList To Array <<
+    private int[] ConvertLinkedListToArray(ListNode head)
+    {
+        List<int> result = [];
+        while (head != null)
+        {
+            result.Add(head.val);
+            head = head.next;
+        }
+
+        return [.. result];
+    }
+    #endregion
+
+
+    #region 21. Merge Two Sorted Lists >>
     [Theory]
     [InlineData(new int[] { 1, 2, 4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
     [InlineData(new int[] { }, new int[] { }, new int[] { })]
@@ -15,30 +44,32 @@ public class LinkedListsTest
         // Arrange
         ListNode list1 = CreateLinkedList(arr1);
         ListNode list2 = CreateLinkedList(arr2);
-        ListNode expected = CreateLinkedList(expectedArr);
 
         // Act
         ListNode result = Solution.MergeTwoLists(list1, list2);
 
         // Assert
-        while (result is not null || expected is not null)
-        {
-            Assert.Equal(expected.val, result!.val);
-            expected = expected.next;
-            result = result.next;
-        }
+        Assert.Equal(expectedArr, ConvertLinkedListToArray(result));
     }
+    #endregion
 
-    private static ListNode CreateLinkedList(int[] arr)
+    #region 83. Remove Duplicates from Sorted List Tests >>
+    [Theory]
+    [InlineData(new int[] { 1, 1, 2 }, new int[] { 1, 2 })]
+    [InlineData(new int[] { 1, 1, 2, 3, 3 }, new int[] { 1, 2, 3 })]
+    [InlineData(new int[] { }, new int[] { })] // Edge case: empty list
+    [InlineData(new int[] { 1 }, new int[] { 1 })] // Edge case: list with one element
+    [InlineData(new int[] { 1, 1, 1 }, new int[] { 1 })] // All elements are duplicates
+    public void DeleteDuplicates_ShouldReturnCorrectList(int[] inputArray, int[] expectedArray)
     {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        foreach (int val in arr)
-        {
-            current.next = new ListNode(val);
-            current = current.next;
-        }
-        return dummy.next;
+        // Arrange
+        ListNode head = CreateLinkedList(inputArray);
+
+        // Act
+        ListNode result = Solution.DeleteDuplicates(head);
+
+        // Assert
+        Assert.Equal(expectedArray, ConvertLinkedListToArray(result));
     }
     #endregion
 }
