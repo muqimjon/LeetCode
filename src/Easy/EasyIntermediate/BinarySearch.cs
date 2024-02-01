36633686d10ddfb35c1f5147451842b1cf972241
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-02-01 21:23:16 +0500

    🔍🚫 278. First Bad Version

---
+++ src/Easy/EasyIntermediate/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿namespace EasyIntermediate;
+
+public class BinarySearch
+{
+    #region 278. First Bad Version
+    /// <summary>
+    /// Finds the first bad version given a range of versions, using binary search.
+    /// </summary>
+    /// <param name="n">The total number of versions.</param>
+    /// <returns>The first bad version.</returns>
+    /// <link>https://leetcode.com/problems/first-bad-version/</link>
+    /// <time>Runtime: O(log n)</time>
+    /// <space>Memory: O(1)</space>
+    public int FirstBadVersion(int n)
+    {
+        int left = 1, right = n;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (IsBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+
+    public static int badVersion;
+    public void SetBadVersion(int bad)
+    {
+        badVersion = bad;
+    }
+
+    public static bool IsBadVersion(int version)
+        => version >= badVersion;
+    #endregion
+}
---
+++ test/Easy/EasyIntermediateTest/BinarySearchTest.cs
@@ -0,0 +1,29 @@
+﻿using EasyIntermediate;
+
+namespace EasyIntermediateTest;
+
+public class BinarySearchTest
+{
+    // Arrange
+    private readonly BinarySearch Solution = new();
+
+    #region 278. First Bad Version
+    [Theory]
+    [InlineData(5, 4, 4)] // Explanation in the prompt
+    [InlineData(1, 1, 1)] // Only one version, and it's bad
+    [InlineData(10, 6, 6)] // Bad version is in the middle
+    [InlineData(7, 7, 7)] // Bad version is the last one
+    [InlineData(8, 3, 3)] // Bad version is early in the sequence
+    public void FirstBadVersion_ShouldReturnCorrectResult(int n, int bad, int expected)
+    {
+        // Arrange
+        Solution.SetBadVersion(bad);
+
+        // Act
+        int result = Solution.FirstBadVersion(n);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    #endregion
+}
