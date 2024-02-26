2e560a16b32250caea0b2daea2b05b63d467466a
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-02-26 22:30:23 +0500

    🔢  461. Hamming Distance

---
+++ src/Easy/EasyIntermediate/BitManipulation.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyIntermediate;
+
+public class BitManipulation
+{
+    #region 461. Hamming Distance
+    /// <summary>
+    /// Calculates the Hamming distance between two integers
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>Hamming distance</returns>
+    /// <link>https://leetcode.com/problems/hamming-distance/</link>
+    /// <time>Time complexity: O(1)</time>
+    /// <space>Space complexity: O(1)</space>
+    public int HammingDistance(int x, int y) =>
+        Convert.ToString(x ^ y, 2).Count(c => c == '1');
+    #endregion
+}
---
+++ test/Easy/EasyIntermediateTest/BitManipulationTest.cs
@@ -0,0 +1,26 @@
+﻿using EasyIntermediate;
+
+namespace EasyIntermediateTest;
+
+public class BitManipulationTest
+{
+    // Arrange
+    private readonly BitManipulation Solution = new();
+
+    #region 461. Hamming Distance
+    [Theory]
+    [InlineData(1, 4, 2)]  // Example 1
+    [InlineData(3, 1, 1)]  // Example 2
+    [InlineData(0, 0, 0)]  // Zero distance
+    [InlineData(255, 255, 0)]  // Same number
+    [InlineData(0, 15, 4)]  // All bits are different
+    public void HammingDistance_ShouldReturnExpectedValue(int x, int y, int expected)
+    {
+        // Act
+        int actual = Solution.HammingDistance(x, y);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+    #endregion
+}
