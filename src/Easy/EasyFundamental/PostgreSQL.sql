964a1f7423b10548bf12c0ac73d810ad6dff12e5
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-02-29 17:06:45 +0500

    🎬  1050. Actors and Directors Who Cooperated At Least Three Times

--- src/Easy/EasyFundamental/PostgreSQL.sql
+++ src/Easy/EasyFundamental/PostgreSQL.sql
@@ -35,3 +35,15 @@ OR conditions LIKE '% DIAB1%';
 SELECT name Customers FROM Customers
 WHERE id NOT IN (SELECT CustomerID FROM Orders);
 ------------------------------------------------------------------------------------
+
+-- 1050. Actors and Directors who cooperated at least three times
+-- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+-- Time Complexity: O(n log n)
+-- Space Complexity: O(N)
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
+------------------------------------------------------------------------------------------------
