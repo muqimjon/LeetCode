cf74c2fb9de6190d5029a8f90240644e349f432f
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-03-03 22:53:04 +0500

    🔄  2000. Reverse Prefix of Word

---
+++ src/Easy/EasyFundamental/TwoPointers.cs
@@ -0,0 +1,24 @@
+﻿namespace EasyFundamental;
+
+public class TwoPointers
+{
+    #region 2000. Reverse Prefix of Word
+    /// <summary>
+    /// Reverses the segment of a word that starts at index 0 and ends at the index of the first occurrence of a specified character (inclusive).
+    /// </summary>
+    /// <param name="word">The input word.</param>
+    /// <param name="ch">The character used to determine the segment to reverse.</param>
+    /// <returns>The word with the specified segment reversed.</returns>
+    /// <link>https://leetcode.com/problems/reverse-prefix-of-word/</link>
+    /// <time>O(n)</time>
+    /// <space>O(n)</space>
+    public string ReversePrefix(string word, char ch)
+    {
+        int index = word.IndexOf(ch);
+        if (index == -1) return word;
+        char[] charArray = word.ToCharArray();
+        Array.Reverse(charArray, 0, index + 1);
+        return new string(charArray);
+    }
+    #endregion
+}
---
+++ test/Easy/EasyFundamentalTest/TwoPointersTest.cs
@@ -0,0 +1,24 @@
+﻿using EasyFundamental;
+
+namespace EasyFundamentalTest;
+
+public class TwoPointersTest
+{
+    // Arrange
+    TwoPointers Solution = new();
+
+    #region 2000. Reverse Prefix of Word
+    [Theory]
+    [InlineData("abcdefd", 'd', "dcbaefd")]
+    [InlineData("xyxzxe", 'z', "zxyxxe")]
+    [InlineData("abcd", 'z', "abcd")]
+    public void TestReversePrefix(string word, char ch, string expected)
+    {
+        // Act
+        string result = Solution.ReversePrefix(word, ch);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    #endregion
+}
