19198945ab44b7d4d1c31dbc0ed086fc6fd9104b
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-04-20 12:00:00 +0500

    🔄  225. Implement Stack using Queues

---
+++ src/Easy/EasyFundamental/Stacks.cs
@@ -0,0 +1,45 @@
+﻿namespace EasyFundamental;
+
+#region 225. Implement Stack using Queues
+/// <summary>
+/// 
+/// </summary>
+public class MyStack
+{
+    private Queue<int> queue;
+    private int topElement;
+
+    public MyStack()
+    {
+        queue = new Queue<int>();
+    }
+
+    public void Push(int x)
+    {
+        queue.Enqueue(x);
+        topElement = x;
+        int size = queue.Count;
+        while (size > 1)
+        {
+            queue.Enqueue(queue.Dequeue());
+            size--;
+        }
+    }
+
+    public int Pop()
+    {
+        int popped = queue.Dequeue();
+        if (queue.Count > 0)
+        {
+            topElement = queue.Peek();
+        }
+        return popped;
+    }
+
+    public int Top()
+        => topElement;
+
+    public bool Empty()
+        => queue.Count == 0;
+}
+#endregion
---
+++ test/Easy/EasyFundamentalTest/StacksTest.cs
@@ -0,0 +1,29 @@
+﻿using EasyFundamental;
+
+namespace EasyFundamentalTest;
+
+public class StacksTest
+{
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(4, 5, 6)]
+    [InlineData(7, 8, 9)]
+    public void MyStack_PushPopTopEmpty(params int[] elements)
+    {
+        MyStack stack = new MyStack();
+
+        foreach (int element in elements)
+        {
+            stack.Push(element);
+            Assert.Equal(element, stack.Top());
+        }
+
+        for (int i = elements.Length - 1; i >= 0; i--)
+        {
+            int popped = stack.Pop();
+            Assert.Equal(elements[i], popped);
+        }
+
+        Assert.True(stack.Empty());
+    }
+}
