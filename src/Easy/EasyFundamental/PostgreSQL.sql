b11e5bf01c44e7a9d684fd181d2604fca39799a7
muqimjon <muqimjon.mr@gmail.com>
2024-06-15 21:54:45 +0500

    🕰️  1741. Find Total Time Spent by Each Employee

--- src/Easy/EasyFundamental/PostgreSQL.sql
+++ src/Easy/EasyFundamental/PostgreSQL.sql
@@ -120,3 +120,17 @@ JOIN Transactions t ON u.account = t.account
 GROUP BY u.name
 HAVING SUM(t.amount) > 10000;
 --------------------------------------------------------------------------------------------
+
+-- 1741. Find Total Time Spent by Each Employee
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+
+-- Time complexity: O(n)
+-- Space complexity: O(n)
+
+SELECT 
+    event_day AS day, 
+    emp_id, 
+    SUM(out_time - in_time) AS total_time
+FROM Employees
+GROUP BY event_day, emp_id;
+--------------------------------------------------------------------------------------------
