a7938a91df70f9811dac6c2e1e9c33040d7da8fc
muqimjon <muqimjon.mr@gmail.com>
2024-10-18 20:43:58 +0500

    🗄️  1795. Rearrange Products Table

--- src/Easy/EasyFundamental/PostgreSQL.sql
+++ src/Easy/EasyFundamental/PostgreSQL.sql
@@ -134,3 +134,26 @@ SELECT
 FROM Employees
 GROUP BY event_day, emp_id;
 --------------------------------------------------------------------------------------------
+
+-- 1795. Rearrange Products Table
+-- https://leetcode.com/problems/rearrange-products-table/
+
+-- Time complexity: O(n)
+-- Space complexity: O(n)
+
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+
+UNION ALL
+
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+
+UNION ALL
+
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL;
+--------------------------------------------------------------------------------------------
