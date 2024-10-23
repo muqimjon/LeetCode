fa35f901778c68d2d1feed67a3445069d8d58c15
muqimjon <muqimjon.mr@gmail.com>
2024-10-23 22:52:59 +0500

    🎉  22. Generate Parentheses

---
+++ src/Medium/MediumFundamental/Strings.cs
@@ -0,0 +1,33 @@
+﻿namespace MediumFundamental;
+
+public class Strings
+{
+    #region 22 Generate Parentheses
+    /// <summary>
+    /// Generates all combinations of well-formed parentheses for n pairs.
+    /// </summary>
+    /// <param name="n">The number of pairs of parentheses.</param>
+    /// <returns>A list of all combinations of well-formed parentheses.</returns>
+    /// <time>O(n^2)</time>
+    /// <space>O(n)</space>
+    public IList<string> GenerateParenthesis(int n)
+    {
+        List<string> result = [];
+
+        void Backtrack(string current, int open, int close)
+        {
+            if (current.Length == n * 2)
+            {
+                result.Add(current);
+                return;
+            }
+
+            if (open < n) Backtrack(current + "(", open + 1, close);
+            if (close < open) Backtrack(current + ")", open, close + 1);
+        }
+
+        Backtrack("", 0, 0);
+        return result;
+    }
+    #endregion
+}
---
+++ test/Medium/MediumFundamentalTest/StringsTest.cs
@@ -0,0 +1,23 @@
+﻿using MediumFundamental;
+
+namespace MediumFundamentalTest;
+
+public class StringsTest
+{
+    // Arrange
+    private readonly Strings Solution = new();
+
+    #region 22 Generate Parentheses
+    [Theory]
+    [InlineData(new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" }, 3)]
+    [InlineData(new string[] { "()" }, 1)]
+    public void TestGenerateParentheses(string[] expected, int n)
+    {
+        // Act
+        var result = Solution.GenerateParenthesis(n);
+
+        // Assert
+        Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+    }
+    #endregion
+}
