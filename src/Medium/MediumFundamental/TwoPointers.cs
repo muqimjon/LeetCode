91ae5ed8786777cdde2b7b1d27b83cdcebda2565
muqimjon <muqimjon.mr@gmail.com>
2024-11-05 15:45:12 +0500

    🔄 151. Reverse Words in a String

---
+++ src/Medium/MediumFundamental/TwoPointers.cs
@@ -0,0 +1,20 @@
+﻿namespace MediumFundamental;
+
+public class TwoPointers
+{
+    #region 151 Reverse Words in a String
+    /// <summary>
+    /// Reverses the order of words in a string with only a single space between them and no leading or trailing spaces.
+    /// </summary>
+    /// <param name="s">The input string.</param>
+    /// <returns>A new string with words in reverse order, separated by a single space.</returns>
+    /// <time>O(n)</time>
+    /// <space>O(n)</space>
+    public string ReverseWords(string s)
+    {
+        var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Array.Reverse(words);
+        return string.Join(" ", words);
+    }
+    #endregion
+}
---
+++ test/Medium/MediumFundamentalTest/TwoPointersTest.cs
@@ -0,0 +1,24 @@
+﻿using MediumFundamental;
+
+namespace MediumFundamentalTest;
+
+public class TwoPointersTest
+{
+    // Arrange
+    private readonly TwoPointers Solution = new();
+
+    #region 151 Reverse Words in a String
+    [Theory]
+    [InlineData("blue is sky the", "the sky is blue")]
+    [InlineData("world hello", "  hello world  ")]
+    [InlineData("example good a", "a good   example")]
+    public void TestReverseWords(string expected, string input)
+    {
+        // Act
+        var result = Solution.ReverseWords(input);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    #endregion
+}
